41fd52263e50b196ae586ad818b06964c3ae1de0
faishalrmdhn <faishalramadhan25@gmail.com>
2020-10-27 11:52:22 +0700

    second commit

---
+++ src/view/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-container>
+    <h1>main page</h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
